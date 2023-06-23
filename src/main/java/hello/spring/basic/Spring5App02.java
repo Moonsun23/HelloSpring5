fc35e32c463460e5a9586c847be96b825de6cbc7
Moonsun23 <socoooool90@gmail.com>
2023-06-23 13:46:57 +0900

    인사말 기능을 인터페이스 기반으로 작성

--- src/main/java/hello/spring/basic/Spring5App02.java
+++ src/main/java/hello/spring/basic/Spring5App02.java
@@ -15,11 +15,11 @@ public class Spring5App02 {
     // 인삿말을 한국어, 영어, 일본어로 출력하도록 기능 추가
 
     public static void main(String[] args){
-        Spring5Bean02 bean02 = new Spring5Bean02();
+        Spring5Bean02 app02 = new Spring5Bean02();
 
-        bean02.sayHelloKor("스프링");
-        bean02.sayHelloEng("Mr.Smith");
-        bean02.sayHelloJpn("나까무라");
+        app02.sayHelloKor("스프링");
+        app02.sayHelloEng("Mr.Smith");
+        app02.sayHelloJpn("나까무라");
     }
 
 
